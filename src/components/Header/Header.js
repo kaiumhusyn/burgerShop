c23b6c7e1a788c1b1c96437f501c14d6dcf684f7
kaiumhusyn <benten0603@gmail.com>
2022-03-26 01:14:32 +0600

    data make done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>The Better Burger</h2>
+            <p><small>Choose Any four burgers</small></p>
+        </div>
+    );
+};
+
+export default Header;
